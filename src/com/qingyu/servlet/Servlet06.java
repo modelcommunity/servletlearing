fdad11e5c293320301fa08ae75782a0b9e17cc89
liujunjie <liujunjiejob@126.com>
2022-01-16 18:09:05 +0800

    获取参数的常用方法

---
+++ src/com/qingyu/servlet/Servlet06.java
@@ -0,0 +1,41 @@
+package com.qingyu.servlet;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/ser06")
+public class Servlet06 extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //获取完整路径
+        System.out.println(req.getRequestURL().toString());
+        System.out.println(req.getRequestURI());
+        System.out.println(req.getQueryString());
+        System.out.println(req.getMethod());
+        System.out.println(req.getProtocol());
+        System.out.println(req.getContextPath());
+
+        /**
+         * 获取请求的参数
+         */
+        //获取制定名称的参数值
+        String name = req.getParameter("name");
+        String pwd = req.getParameter("pwd");
+        System.out.println("name： " + name);
+        System.out.println("pwd： " + pwd);
+        //http://localhost:8080/s01/ser06?name=admin&pwd=123&hobby=sing&hobby=dance
+        //获取制定名称的参数的所有参数值（用于复选框）
+        String[] hobbys = req.getParameterValues("hobby");
+        if (hobbys != null && hobbys.length > 0) {
+            for (String hobby : hobbys) {
+                System.out.println("爱好："+hobby);
+            }
+        }
+    }
+}
