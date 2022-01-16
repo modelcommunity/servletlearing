3b6cc084e1b224710ced915e564416e58aa24b1f
liujunjie <liujunjiejob@126.com>
2022-01-16 17:34:41 +0800

    多种实现方式

---
+++ src/com/qingyu/servlet/Servlet04.java
@@ -0,0 +1,26 @@
+package com.qingyu.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/ser04")
+public class Servlet04 extends HttpServlet {
+//    @Override
+//    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.service(req, resp);
+//    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Get请求");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Post请求");
+        doGet(req,resp);
+    }
+}
---
+++ src/com/qingyu/servlet/servlet02.java
@@ -0,0 +1,15 @@
+package com.qingyu.servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+@WebServlet("/ser02")
+public class servlet02 extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("继承GenericServlet");
+    }
+}
